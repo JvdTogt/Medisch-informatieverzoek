4740a3b99eef966889419c0e4e3c2e2f61b112c3
JvdTogt <jorisvandertogt@gmail.com>
2026-02-19 13:17:27 +0100

    Delete constants.ts

--- constants.ts
+++
@@ -1,7 +0,0 @@
-
-export const MAX_A4_CHARS = 3000; // Rough estimate for a single A4 page
-export const MIME_TYPES = {
-  DOCX: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-  IMAGE_JPEG: "image/jpeg",
-  IMAGE_PNG: "image/png"
-};
